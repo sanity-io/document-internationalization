3845a37f98caab8273deb599c56a04afa02583ac
Liam Martens <hi@liammartens.com>
2021-03-14 14:32:36 +0100

    Updated object input

--- src/types/IType.ts
+++ src/types/IType.ts
@@ -1,13 +1,31 @@
 import { TLanguagesOption } from './TLanguagesOption';
+import { ObjectSchemaTypeWithOptions } from '@sanity/types';
+export interface IField {
+    name: string;
+    type: IType;
+}
+
+export interface IFieldSet {
+    name: string;
+    title: string;
+    description: string;
+    single: boolean;
+    options: ObjectSchemaTypeWithOptions['options'];
+    field: IField;
+    fields: IField[];
+}
 
 export interface IType {
     name: string;
+    title: string;
+    description: string;
     hidden?: boolean;
     level: number;
     type: string;
     jsonType: string;
-    fields: any[];
-    options: {
+    fields: IField[];
+    fieldsets: IFieldSet[];
+    options:  ObjectSchemaTypeWithOptions['options'] & {
         base?: string;
         i18n?: boolean;
         languages?: TLanguagesOption;
