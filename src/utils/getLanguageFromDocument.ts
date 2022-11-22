f302151cba3c6c17f3bec272492b8f55cfc8c32f
Snorre Eskeland Brekke <snorre.e.brekke@gmail.com>
2022-11-22 14:38:33 +0100

    feat!: initial rewrite of plugin
    BREAKING CHANGE: API has changed, consult README for details

--- src/utils/getLanguageFromDocument.ts
+++
@@ -1,9 +0,0 @@
-import type {SanityDocument} from '@sanity/client'
-import type {ApplyConfigResult} from './applyConfig'
-
-export function getLanguageFromDocument(
-  doc: SanityDocument,
-  config: ApplyConfigResult
-): string | null {
-  return doc?.[config.fieldNames.lang] || config.base || null
-}
