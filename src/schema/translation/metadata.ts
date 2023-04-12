963b51f251efeb2e8403caf7cbbebad978c69ac6
Simeon Griggs <simeon@hey.com>
2023-04-12 11:23:56 +0100

    fix: lint and satisfy type

--- src/schema/translation/metadata.ts
+++ src/schema/translation/metadata.ts
@@ -1,11 +1,9 @@
-import {defineType, defineField} from 'sanity'
 import {TranslateIcon} from '@sanity/icons'
+import {defineField, defineType, DocumentDefinition} from 'sanity'
 
 import {METADATA_SCHEMA_NAME} from '../../constants'
 
-// TODO: TS having a hard time determining the return type here. Likely a V3 type problem.
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export default (schemaTypes: string[]) =>
+export default (schemaTypes: string[]): DocumentDefinition =>
   defineType({
     type: 'document',
     name: METADATA_SCHEMA_NAME,
@@ -22,6 +20,8 @@ export default (schemaTypes: string[]) =>
         description:
           'Used to filter the reference fields above so all translations share the same types.',
         type: 'array',
+        // For some reason TS dislikes this line because of the DocumentDefinition return type
+        // @ts-expect-error
         of: [{type: 'string'}],
         options: {list: schemaTypes},
         readOnly: ({value}) => Boolean(value),
@@ -35,9 +35,13 @@ export default (schemaTypes: string[]) =>
       prepare(selection) {
         const {translations, documentSchemaTypes} = selection
         const title =
-          translations.length === 1 ? `1 Translation` : `${translations.length} Translations`
+          translations.length === 1
+            ? `1 Translation`
+            : `${translations.length} Translations`
         const languageKeys = translations.length
-          ? translations.map((t: {_key: string}) => t._key.toUpperCase()).join(', ')
+          ? translations
+              .map((t: {_key: string}) => t._key.toUpperCase())
+              .join(', ')
           : ``
         const subtitle = [
           languageKeys ? `(${languageKeys})` : null,
