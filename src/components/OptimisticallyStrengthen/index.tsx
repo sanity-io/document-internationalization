03af0d12dc6e8a20ff265f1e10b02aa90f27a1ca
Simeon Griggs <simeon@hey.com>
2023-04-13 17:15:16 +0100

    fix: type import

--- src/components/OptimisticallyStrengthen/index.tsx
+++ src/components/OptimisticallyStrengthen/index.tsx
@@ -1,4 +1,4 @@
-import {TranslationReference} from '../BulkPublish'
+import {TranslationReference} from '../../types'
 import ReferencePatcher from './ReferencePatcher'
 
 type OptimisticallyStrengthenProps = {
