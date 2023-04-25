8a93ba1594090c7bea98c366d78ef23a608c174f
Simeon Griggs <simeon@hey.com>
2023-04-25 09:20:10 +0100

    chore: lint

--- src/utils/createReference.ts
+++ src/utils/createReference.ts
@@ -1,20 +1,20 @@
-import { TranslationReference } from "../types";
+import {TranslationReference} from '../types'
 
 export function createReference(
-    key: string,
-    ref: string,
-    type: string
-  ): TranslationReference {
-    return {
-      _key: key,
-      _type: 'internationalizedArrayReferenceValue',
-      value: {
-        _type: 'reference',
-        _ref: ref,
-        _weak: true,
-        _strengthenOnPublish: {
-          type,
-        },
+  key: string,
+  ref: string,
+  type: string
+): TranslationReference {
+  return {
+    _key: key,
+    _type: 'internationalizedArrayReferenceValue',
+    value: {
+      _type: 'reference',
+      _ref: ref,
+      _weak: true,
+      _strengthenOnPublish: {
+        type,
       },
-    }
+    },
   }
+}
