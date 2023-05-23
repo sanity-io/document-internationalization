6ba7d2fec2aff21ddc8545dc6ee0359432204809
Simeon Griggs <simeon@hey.com>
2023-05-23 12:42:43 +0100

    chore: add migration guide

--- migrations/createMetadata.ts
+++ migrations/createMetadata.ts
@@ -1,49 +1,58 @@
-import sanityClient from '@sanity/client'
 import {SanityDocumentLike} from 'sanity'
+import {getCliClient} from 'sanity/cli'
 
-// This migration script is expecting your documents
-// to have a field structure like this:
-// And will create new `translation.metadata` documents
-// With a similar shape
+/**
+ * This migration script creates new `translation.metadata` documents for all
+ * documents that have a `__i18n_refs` field that is an array of references.
+ *
+ * This migration is necessary for the new version of the plugin to work.
+ *
+ * 1. Take a backup of your dataset with:
+ * `npx sanity@latest dataset export`
+ *
+ * 2. Copy this file to the root of your Sanity Studio project
+ *
+ * 3. Update the `UNSET_REFS_FIELD`, `UNSET_BASE_FIELD`,
+ * `SCHEMA_TYPE` and `WEAK_REFS` constants to match your use
+ *
+ * 4. Run the script (replace <schema-type> with the name of your schema type):
+ * npx sanity@latest exec ./createMetadata.ts --with-user-token
+ *
+ * 5. Repeat for every schema type and dataset using the updated plugin
+ */
 
-//   __i18n_refs: [
-//     {
-//       _key: 'en_GB',
-//       _ref: '32cb7277-1cfb-4ab1-ae1e-ab844dc8ab72__i18n_en_GB',
-//       _type: 'reference',
-//     },
-//     {
-//       _key: 'nl',
-//       _ref: '32cb7277-1cfb-4ab1-ae1e-ab844dc8ab72__i18n_nl',
-//       _type: 'reference',
-//     },
-//     {
-//       _key: 'no',
-//       _ref: '32cb7277-1cfb-4ab1-ae1e-ab844dc8ab72__i18n_no',
-//       _type: 'reference',
-//     },
-//   ],
-
-const client = sanityClient({
-  projectId: '6h1mv88x',
-  dataset: 'production-v3',
-  apiVersion: `2022-06-01`,
-  // eslint-disable-next-line no-process-env
-  token: process.env.SANITY_TOKEN,
-})
+// This will use the client configured in ./sanity.cli.ts
+const client = getCliClient()
 
 // Values in this field will be used to create meta documents
 const UNSET_REFS_FIELD = `__i18n_refs`
 // This field will be unset from all documents that contain it
 const UNSET_BASE_FIELD = `__i18n_base`
+// This field will NOT be modified in this script
+// But may be customized in your existing plugin config
+// Run the `renameLanguageField.ts` script after if you want to change this
+const LANGUAGE_FIELD = `__i18n_lang`
 // Operation will be scoped to just this one document type
 const SCHEMA_TYPE = `lesson`
 
+// Make references to translated documents weak?
+const WEAK_REFS = false
+
+// eslint-disable-next-line no-console
+console.log(
+  `Finding "${SCHEMA_TYPE}" documents with translation references in a "${UNSET_REFS_FIELD}" field to create "translation.metadata" documents.`
+)
+
 const fetchDocuments = () =>
   client.fetch(
-    `*[_type == $type && (defined(${UNSET_REFS_FIELD}) || defined(${UNSET_BASE_FIELD}))][0...100] {
+    `*[
+      _type == $type 
+      && (defined(${UNSET_REFS_FIELD}) || defined(${UNSET_BASE_FIELD})) 
+      && defined(${LANGUAGE_FIELD})
+    ][0...100] {
       _id, 
-      _rev, 
+      _rev,
+      ${LANGUAGE_FIELD},
       ${UNSET_REFS_FIELD}, 
     }`,
     {type: SCHEMA_TYPE}
@@ -73,13 +82,24 @@ const buildMetadata = (docs: DocumentWithRefs[]) => {
     .filter((doc) => doc?.[UNSET_REFS_FIELD]?.length)
     .map((doc) => ({
       _type: 'translation.metadata',
-      translations: doc[UNSET_REFS_FIELD].map(({_ref, _key}) => ({
-        _key,
-        value: {
-          _type: 'reference',
-          _ref,
+      translations: [
+        {
+          _key: doc[LANGUAGE_FIELD],
+          value: {
+            _type: 'reference',
+            _ref: doc._id.replace(`drafts.`, ``),
+            ...(WEAK_REFS ? {_weak: true} : {}),
+          },
         },
-      })),
+        ...doc[UNSET_REFS_FIELD].map(({_ref, _key}) => ({
+          _key,
+          value: {
+            _type: 'reference',
+            _ref,
+            ...(WEAK_REFS ? {_weak: true} : {}),
+          },
+        })),
+      ],
     }))
 }
 
@@ -109,6 +129,10 @@ const migrateNextBatch = async () => {
   if (patches.length === 0) {
     // eslint-disable-next-line no-console
     console.debug('No more documents to create or patch!')
+    // eslint-disable-next-line no-console
+    console.debug(
+      'Be sure to migrate your "language" field using the "renameLanguageField.ts" script or update your plugin configuration\'s "Langage Field" setting'
+    )
     return null
   }
 
--- migrations/renameLanguageField.ts
+++ migrations/renameLanguageField.ts
@@ -1,25 +1,41 @@
-import sanityClient from '@sanity/client'
 import {SanityDocumentLike} from 'sanity'
+import {getCliClient} from 'sanity/cli'
 
-const client = sanityClient({
-  projectId: '6h1mv88x',
-  dataset: 'production-v3',
-  apiVersion: `2022-06-01`,
-  // eslint-disable-next-line no-process-env
-  token: process.env.SANITY_TOKEN,
-})
+/**
+ * This migration script rewrites the value of field `__i18n_lang` to `language`
+ * on all documents of type `lesson`, overwriting the `language` field if it existed.
+ *
+ * This migration is NOT necessary for the new version of the plugin but you may
+ * prefer to no longer have field names with double underscore prefixes.
+ *
+ * 1. Take a backup of your dataset with:
+ * `npx sanity@latest dataset export`
+ *
+ * 2. Copy this file to the root of your Sanity Studio project
+ *
+ * 3. Update the `UNSET_FIELD_NAME`, `NEW_FIELD_NAME` and `SCHEMA_TYPE` constants
+ * to match your use current data if you customised these settings in the
+ * previous versions of the plugin
+ *
+ * 4. Run the script (replace <schema-type> with the name of your schema type):
+ * npx sanity@latest exec ./renameLanguageField.ts --with-user-token
+ *
+ * 5. Repeat for every schema type and dataset using the updated plugin
+ */
 
 const UNSET_FIELD_NAME = `__i18n_lang`
 const NEW_FIELD_NAME = `language`
 const SCHEMA_TYPE = `lesson`
+
+// This will use the client configured in ./sanity.cli.ts
+const client = getCliClient()
 
 const fetchDocuments = () =>
   client.fetch(
     `*[_type == $type && defined(${UNSET_FIELD_NAME})][0...100] {
       _id, 
       _rev, 
-      ${UNSET_FIELD_NAME}, 
-      ${NEW_FIELD_NAME}
+      ${UNSET_FIELD_NAME}
     }`,
     {type: SCHEMA_TYPE}
   )
@@ -30,8 +46,8 @@ const buildPatches = (docs: SanityDocumentLike[]) =>
     patch: {
       set: {[NEW_FIELD_NAME]: doc[UNSET_FIELD_NAME]},
       unset: [UNSET_FIELD_NAME],
-      // this will cause the migration to fail if any of the documents has been
-      // modified since it was fetched.
+      // this will cause the migration to fail if any of the
+      // documents have been modified since the original fetch.
       ifRevisionID: doc._rev,
     },
   }))
